58f0e9e259c06eda385fa11098d3ee7a90e6f608
Choeles Tung <choelestung@gmail.com>
2018-06-06 12:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function takeANumber(katzDeliLine, name)  {
 function nowServing (katzDeliLine)  {
   if(katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine.shift()}.`;
-    
   }
   else{
     return "There is nobody waiting to be served!";
@@ -32,5 +31,33 @@ function currentLine(katzDeliLine)  {
   }
   else{
     return "The line is currently empty.";
+  }
+}
+
+var counter = 0;
+var toBeServ = 0;
+
+function takeANumber() {
+  counter++;
+  return `Welcome you are number ${counter}`;
+}
+
+function nextToServe () {
+  if(counter > toBeServe)  {
+    toBeServed++
+    return toBeServe;
+  }
+  else{
+    return "no 1"
+  }
+}
+
+
+function nowServing (katzDeliLine)  {
+  if(counter > 0){
+    return `Currently serving ${counter}.`;
+  }
+  else{
+    return "There is nobody waiting to be served!";
   }
 }
